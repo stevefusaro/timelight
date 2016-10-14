135f86874d94b1b2eff46afc7ee7745910a14cf6
Steve Fusaro <sfusaro1@gmail.com>
2016-10-14 01:05:41 -0400

    Renamed Django root dir from "project" to "app"

--- app/project/settings.py
+++ app/project/settings.py
@@ -69,14 +69,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'timelight',
+        'USER': 'timelight',
+        'PASSWORD': 'timelight',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
@@ -105,6 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
+# https://docs.djangoproject.com/en/1.10/topics/i18n/timezones/#postgresql
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
