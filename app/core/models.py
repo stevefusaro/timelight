6ed2947c1725dbf857712cae6fdcb30f96f30aee
Steve Fusaro <sfusaro1@gmail.com>
2016-10-20 19:46:50 -0400

    Add an Event model so I can play around with tests

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=256)
+    timestamp = models.DateTimeField(null=False)
+    created_at = models.DateTimeField(null=False, auto_now_add=True)
+    updated_at = models.DateTimeField(null=False, auto_now=True)
+
+# Store event dates in a different model that captures the nature of the time, e.g.
+# Target date vs appointment date vs date range
