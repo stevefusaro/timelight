8c287b79a61de4cea2552201beb33ee5c62ec0bc
Steve Fusaro <sfusaro1@gmail.com>
2017-01-10 23:47:11 -0500

    Navigation without React Bootstrap

--- front/src/App.js
+++ front/src/App.js
@@ -1,53 +1,38 @@
 import React, { Component } from 'react';
-import { Grid, Navbar, Jumbotron, Button, Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar inverse fixedTop>
-          <Grid>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="/">TimeLight</a>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-            </Navbar.Header>
-            <Nav>
-              <NavItem eventKey={2} href="#">Dashboard</NavItem>
-              <NavItem eventKey={1} href="#">Views</NavItem>
-              <NavItem eventKey={2} href="#">Discover</NavItem>
-              <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                <MenuItem eventKey={3.1}>Action</MenuItem>
-                <MenuItem eventKey={3.2}>Another action</MenuItem>
-                <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                <MenuItem divider />
-                <MenuItem eventKey={3.3}>Separated link</MenuItem>
-              </NavDropdown>
-            </Nav>
-          </Grid>
-        </Navbar>
+      <div className="App">
 
-        <Jumbotron>
-          <Grid>
-            <h1>Welcome to React</h1>
-            <p>
-              <Button
-                bsStyle="success"
-                bsSize="large"
-                href="http://react-bootstrap.github.io/components.html"
-                target="_blank">
-                View React Bootstrap Docs
-              </Button>
-            </p>
-          </Grid>
-        </Jumbotron>
+        <nav className="navbar navbar-inverse navbar-fixed-top">
+          <div className="container">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="#">Project name</a>
+            </div>
+            <div id="navbar" className="collapse navbar-collapse">
+              <ul className="nav navbar-nav">
+                <li className="active"><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
 
+        <div className="container">
+            To get started, edit <code>src/App.js</code> and save to reload.
+        </div>
 
       </div>
-
-
-
     );
   }
 }
