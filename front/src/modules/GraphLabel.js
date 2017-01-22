92e49bcb2aa0e5e2cb9af4275db6222fe0f5907d
Steve Fusaro <sfusaro1@gmail.com>
2017-01-21 21:32:53 -0500

    Front-end showing list of nodes

--- front/src/modules/GraphLabel.js
+++ front/src/modules/GraphLabel.js
@@ -1,12 +1,40 @@
-// modules/Repo.js
 import React from 'react'
 
-export default React.createClass({
+
+class GraphLabel extends React.Component{
+
+  constructor() {
+    super();
+    this.state = { nodes: [] };
+  }
+
+  componentDidMount() {
+    this.getNodes();
+  }
+
+  getNodes() {
+    const url = 'http://localhost:8000/graph/label_nodes';
+    fetch(url,)
+      .then(response => response.json())
+      .then(responseJson => 
+        this.setState({nodes:responseJson})
+      )
+      .catch(error => console.error(error));
+  }
+
   render() {
     return (
       <div>
         <h2>Label: {this.props.params.labelName}</h2>
+        <h4>Nodes</h4>
+        <ul>
+          {this.state.nodes.map(node =>
+            <li>{node.person.first_name}</li>
+          )}
+        </ul>
       </div>
     )
   }
-})
+}
+
+export default GraphLabel;
--- front/src/modules/GraphNodeList.js
+++ front/src/modules/GraphNodeList.js
@@ -1,7 +1,25 @@
 class GraphNodeList extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { nodes: [] };
+  }
+
+  componentDidMount() {
+    this.getNodes();
+  }
+
+  getNodes() {
+    const url = 'http://127.0.0.1:8000/graph/label_nodes';
+    fetch(url)
+      .then((response) => response.json())
+      .then((responseJson) => this.setState({nodes:responseJson}))
+      .catch((error) => console.error(error));
+  }
+
   render() {
     return (
-      <div className="graph-node-list">
+      <div>
         <h1>Nodes for Label {this.props.label}</h1>
         <ul>
           <li>Node A</li>
