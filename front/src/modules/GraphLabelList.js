d8b9d17b80aa45d9890688cc2e2ee0a72c4d5760
Steve Fusaro <sfusaro1@gmail.com>
2017-01-21 19:23:08 -0500

    Dynamically getting list of node labels

--- front/src/modules/GraphLabelList.js
+++ front/src/modules/GraphLabelList.js
@@ -3,33 +3,35 @@ import { Link } from 'react-router'
 
 class GraphLabelList extends React.Component {
 
-  getLabels() {
-    let headers = {
-        'Authorization': 'Basic bmVvNGo6ZXpwYXNz' //  + btoa('username:password'), 
-      }
-    return fetch('http://localhost:7474/db/data/labels', {headers: headers})
-      .then((response) => response.json())
-      .then((responseJson) => {
-        console.log(responseJson)
-        return responseJson;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+  constructor() {
+    super();
+    this.state = { labels: ["first"] };
   }
 
-  bark() {
-    this.getLabels()
-    return <span>WOOF</span>
+  componentDidMount() {
+    this.getLabels();
+  }
+
+  getLabels() {
+    const url = 'http://localhost:7474/db/data/labels';
+    const headers = {'Authorization': 'Basic bmVvNGo6ZXpwYXNz'}; //  + btoa('username:password')
+    fetch(url, { headers })
+      .then((response) => response.json())
+      .then((responseJson) => this.setState({labels:responseJson}))
+      .catch((error) => console.error(error));
   }
 
   render() {
     return (
       <div className="graph-label-list">
-        <b>Labels</b>
+        <b>Node Labels</b>
+
         <ul>
-          <li><Link to="/graph/label/Person">Person  {this.bark()} </Link></li>
-          <li><Link to="/graph/label/Company">Company</Link></li>
+          {this.state.labels.map(label =>
+            <li key={label}>
+              <Link to="/graph/label/:{label}">{label}</Link>
+            </li>
+          )}
         </ul>
 
         {this.props.children}
